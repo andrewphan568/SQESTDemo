bb6850655989de41f7aaa480fd8c03a5bc5aa1ae
Andrew Phan <andrewphan568@gmail.com>
2021-08-14 09:49:14 +0930

    add MoistureContent model object

---
+++ Domain/Abstractions/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Abstractions
+{
+    public interface IEntityBase
+    {
+        DateTime CreatedAtUtc { get; set; }
+        DateTime UpdatedAtUtc { get; set; }
+       // User CreatedBy { get; set; }
+       // User UpdatedBy { get; set; }
+    }
+}
---
+++ Domain/Moisture/MoistureContent.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstractions;
+using System;
+
+namespace Domain.Moisture
+{
+    public class MoistureContent : IEntityBase
+    {
+        public Guid Id { get; set; }
+        public string TareId { get; set; }
+        public double TareMass { get; set; }
+        public double TareAndMaterialWetMass { get; set; }
+
+        public string Balance { get; set; }
+        public double TareAndMaterialDryMass { get; set; }
+        public double MaterialDryMass { get; set; }
+
+        public double WaterContent { get; set; }
+        public bool SelectInsufficientSampleMass { get; set; }
+        public bool SelectDryingTemperature { get; set; }
+        public bool SelectMaterialExcluded { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAtUtc { get; set; }
+    }
+}
