94eb45d74a8417e55908643e564954b4d651ebee
Andrew Phan <andrewphan568@gmail.com>
2021-08-15 10:15:58 +0930

    add MoistureContentList Page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './styles.css';
 import NavBar from './NavBar';
 import MoistureContentDetails from '../../features/worksheet/moistureContentDetails';
+import MoistureContentList from '../../features/worksheet/moistureContentList'
 import { Container } from 'semantic-ui-react';
 function App() {
+
+  const [moistureContents, setMoistureContents] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/moistureContent').then(response => {
+      console.log(response);
+      setMoistureContents(response.data.value);
+    })
+  }, [])
+
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <MoistureContentDetails />
+        <MoistureContentList moistureContents={moistureContents} />
       </Container>
 
     </>
---
+++ client-app/src/features/worksheet/moistureContentList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Item, Segment } from 'semantic-ui-react';
+import { MoistureContent } from '../../app/models/apiTypes';
+
+interface Props {
+    moistureContents: MoistureContent[];
+}
+
+export default function MoistureContentList({ moistureContents }: Props) {
+    return (
+        <Segment style={{ width: '60%' }}>
+            <Item.Group divided>
+                {moistureContents.map(moistureContent => (
+                    <Item key={moistureContent.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{'Project ' + moistureContent.project?.name}</Item.Header>
+                            <Item.Description>
+                                <div>{'Water Content (%): ' + moistureContent.waterContentPercentage + ' (%)'}</div>
+                                <div>{'Tested By: ' + moistureContent.testerName}</div>
+                                <div>{'Date Tested: ' + moistureContent.dateTested}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button floated='right' content='View' color='blue' />
+                                <Button floated='right' content='Delete' color='red' />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+}
